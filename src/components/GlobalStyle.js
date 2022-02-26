4f5af2b1f8cd55ecbb5c6284378cecd9a7dc1647
geeonie <nomad.jiwhan@gmail.com>
2022-02-27 02:12:57 +0900

    feature: created Logo component

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  :root {
+    --magnolia-color: #F4EEFF;
+    --pale-lavender-color: #DCD6F7;
+    --maximum-blue-purple-color: #F4EEFF;
+    --independence-color: #424874;
+  }
+
+  html {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    background-color: var(--independence-color);
+  }
+
+  body {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    min-width: 320px;
+    max-width: 430px;
+    height: 100vh;
+    min-height: calc(100vh - 5rem);
+    font-family: "Noto Sans KR", sans-serif;
+    -ms-overflow-style: none;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  #root {
+    height: 100%;
+  }
+`;
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+
+import GlobalStyle from "./components/GlobalStyle";
+import Login from "./pages/Login";
+
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <Login />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 reportWebVitals(console.log);
