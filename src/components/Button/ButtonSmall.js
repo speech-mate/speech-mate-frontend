ac8fdf78e52f7afea86f429aa6ae705d2f2f3501
geeonie <nomad.jiwhan@gmail.com>
2022-03-03 01:59:41 +0900

    feature: added speech setting subpage on new practice page

--- src/components/Button/ButtonSmall.js
+++ src/components/Button/ButtonSmall.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import propTypes from "prop-types";
 
-function ButtonSmall() {
-  return <div>ButtonSmall</div>;
+function ButtonSmall({ size, text, onClick }) {
+  return (
+    <SmallButton size={size} onClick={onClick}>
+      {text}
+    </SmallButton>
+  );
 }
+
+const SmallButton = styled.button`
+  width: ${(props) => props.size.width};
+  height: ${(props) => props.size.height};
+  border: none;
+  border-radius: 4px;
+  background-color: var(--maximum-blue-purple-color);
+  color: white;
+  font-size: 16px;
+`;
+
+ButtonSmall.propTypes = {
+  size: propTypes.object,
+  text: propTypes.string,
+  onClick: propTypes.func,
+};
 
 export default ButtonSmall;
